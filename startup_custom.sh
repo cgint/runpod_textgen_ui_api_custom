994ef48eb09facb8af1bd6af8bd26e41db1ea398
cgi <christian.gintenreiter@smarter-ecommerce.com>
2023-11-26 20:51:04 +0100

    Fix script path

--- startup_custom.sh
+++ startup_custom.sh
@@ -2,7 +2,7 @@
 
 # Install ollama so we have the newest version and start the server
 # Do it in background so startup of web-ui is not slowed down
-./init_ollama.sh &
+/init_ollama.sh &
 
 # Start the original script by TheBloke to run the original procedure
-./start.sh
+/start.sh
