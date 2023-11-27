edbb8ec7bed12ee811956a9861e222c2056377d5
Christian Gintenreiter <cgint@users.noreply.github.com>
2023-11-27 20:05:25 +0100

    Update init_ollama.sh
    Preloading ollama models

--- init_ollama.sh
+++ init_ollama.sh
@@ -8,3 +8,19 @@ sleep 1
 
 echo "Here are the installed ollama packages:"
 ollama list
+
+# Check if OLLAMA_PULL is set and not empty
+if [ -n "$OLLAMA_PULL" ]; then
+    echo "Preloading ollama models $OLLAMA_PULL"
+
+    # Convert the comma-separated string into an array
+    IFS=',' read -ra ADDR <<< "$OLLAMA_PULL"
+
+    # Iterate over the array and run 'ollama pull' for each item
+    for i in "${ADDR[@]}"; do
+        echo "Running 'ollama pull' for $i"
+        ollama pull "$i"
+    done
+else
+    echo "OLLAMA_PULL is not set or empty"
+fi
